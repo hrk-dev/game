5964dd92b6863abdf7d5a1ed9400e767959b79bf
blacktunes <blacktunes@foxmail.com>
2021-06-09 16:10:20 +0800

    feat: 更新框架和插件

---
+++ src/js/server/socket.js
@@ -0,0 +1,19 @@
+// 该文件正式版需要修改相应事件和开关ID
+
+// var socket = io('http://127.0.0.1?type=game&key=zheshimima')
+
+// socket.disconnect()
+
+// socket.on('connect', () => {
+//   console.log('已连接至服务器')
+// })
+
+// Object.defineProperty(SceneManager, '_scene', {
+//   set: (scene) => {
+//     SceneManager.__scene = scene
+//     socket.emit('set-start', scene.constructor.name === 'Scene_Map')
+//   },
+//   get: () => {
+//     return SceneManager.__scene
+//   }
+// })
