7bdf23c49ebdc4c767a0e04e64a6df15b42bc1b1
blacktunes <blacktunes@foxmail.com>
2021-10-22 15:49:33 +0800

    perf: 移除无用插件

--- src/js/rm_patch/rpg_scenes.js
+++ src/js/rm_patch/rpg_scenes.js
@@ -1,4 +1,34 @@
+(function () {
+  const Tip = Scene_Map.prototype.start
+  Scene_Map.prototype.start = function () {
+    if (!$gameSystem.tipData) {
+      $gameSystem.tipData = {
+        show: false,
+        en: '',
+        cn: '',
+        time: 2000
+      }
+    }
+    Tip.call(this)
+  }
+})()
+
 /** 禁用鼠标移动 */
-Scene_Map.prototype.isMapTouchOk = function() {
+Scene_Map.prototype.isMapTouchOk = function () {
   return (this.isActive() && $gamePlayer.canMove()) && !($gameSystem._savedZoom && $gameSystem._savedZoom.follow)
+}
+
+/** VUE主菜单适配 */
+Scene_Title.prototype.create = function () {
+  return
+}
+Scene_Title.prototype.start = function () {
+  VueMain.app.$refs.MainMenu.show = true
+  return
+}
+Scene_Title.prototype.update = function () {
+  return
+}
+Scene_Title.prototype.isBusy = function () {
+  return false
 }
