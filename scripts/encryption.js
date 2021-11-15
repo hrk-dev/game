e3f4a1bf0e0f01b20d5e5c86f66c8696eff19ea8
blacktunes <blacktunes@foxmail.com>
2021-11-15 22:19:53 +0800

    feat: 优化加密脚本

--- scripts/encryption.js
+++ scripts/encryption.js
@@ -2,13 +2,25 @@ const fs = require('fs-extra')
 const path = require('path')
 const md5 = require('md5-js')
 
+function md5Dir(dir, name) {
+  const oldDir = path.join(dir, name)
+  const newDir = path.join(oldDir, `../${md5(name)}`)
+  fs.renameSync(oldDir, newDir)
+  console.log(`  ${oldDir}\n→ ${newDir}\n`)
+  md5Filename(newDir)
+}
+
 function md5Filename(dir) {
   fs.readdirSync(dir).forEach(file => {
     let fullPath = path.join(dir, file)
+    const newPath = path.join(fullPath, `../${md5(file)}`)
     if (fs.lstatSync(fullPath).isDirectory()) {
-      md5Filename(fullPath)
+      fs.renameSync(fullPath, newPath)
+      console.log(`  ${fullPath}\n→ ${newPath}\n`)
+      md5Filename(newPath)
     } else {
-      fs.renameSync(fullPath, fullPath.replace(file, md5(file)))
+      console.log(`  ${fullPath}\n→ ${newPath}\n`)
+      fs.renameSync(fullPath, newPath)
     }
   })
 }
@@ -35,20 +47,7 @@ console.log(`复制文件结束 ${((Date.now() - s) / 1000).toFixed(2)}s`)
 s = Date.now()
 console.log('开始加密文件')
 
-const audioDir = path.join(dir, 'audio')
-md5Filename(audioDir)
-const audioList = fs.readdirSync(audioDir)
-audioList.forEach(name => {
-  fs.renameSync(path.join(audioDir, name), path.join(audioDir, md5(name)))
-})
-fs.renameSync(audioDir, path.join(dir, md5('audio')))
+md5Dir(dir, 'audio')
+md5Dir(dir, 'img')
 
-const imgDir = path.join(dir, 'img')
-md5Filename(path.join(dir, 'img'))
-const imgList = fs.readdirSync(imgDir)
-imgList.forEach(name => {
-  fs.renameSync(path.join(imgDir, name), path.join(imgDir, md5(name)))
-})
-fs.renameSync(imgDir, path.join(dir, md5('img')))
-
-console.log(`加密文件完成 ${((Date.now() - s) / 1000).toFixed(2)}s`)
+console.log(`加密文件完成 ${((Date.now() - s) / 1000).toFixed(2)}s\n`)
