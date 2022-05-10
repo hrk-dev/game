7c80fc73061d7a59a4f65be7846c66615577c41b
blacktunes <blacktunes@foxmail.com>
2022-05-10 15:22:38 +0800

    feat: 优化Debug组件

--- src/game/js/vue/components/Temp/Debug.vue
+++ src/game/js/vue/components/Temp/Debug.vue
@@ -1,5 +1,30 @@
 <template>
   <div id="debug">
+    <div class="error" title="存在素材缺失，请在菜单Debug页查看" v-if="dev && list.length > 0">
+      <svg
+        viewBox="0 0 1024 1024"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+      >
+        <path
+          d="M512 720m-48 0a48 48 0 1 0 96 0 48 48 0 1 0-96 0Z"
+          p-id="1939"
+          fill="#FFB0AA"
+        ></path>
+        <path
+          d="M480 416v184c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V416c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8z"
+          p-id="1940"
+          fill="#FFB0AA"
+        ></path>
+        <path
+          d="M955.7 856l-416-720c-6.2-10.7-16.9-16-27.7-16s-21.6 5.3-27.7 16l-416 720C56 877.4 71.4 904 96 904h832c24.6 0 40-26.6 27.7-48z m-783.5-27.9L512 239.9l339.8 588.2H172.2z"
+          p-id="1941"
+          fill="#FFB0AA"
+        ></path>
+      </svg>
+    </div>
     <transition name="fade">
       <div class="debug-wrapper" v-if="isShow && dev">
         <div class="line">
@@ -58,7 +83,7 @@ module.exports = {
       this.isShow = false
     },
     add(name) {
-      const msg = `${$dataMapInfos[$gameMap.mapId()].name} - 事件${$gameMap._interpreter.eventId()}`
+      const msg = `${$dataMapInfos[$gameMap.mapId()].name}(${$gameMap.mapId()}) - ${`${$dataMap.events[$gameMap._interpreter.eventId()].name}(${$gameMap._interpreter.eventId()})` || `事件${$gameMap._interpreter.eventId()}`}`
       if (!this.event.includes(msg)) {
         console.warn(`${msg} 缺少立绘`)
         this.event.push(msg)
@@ -90,6 +115,7 @@ module.exports = {
             Components.Loading.loadingHide()
             Patch.showTip()
             Patch.stopWait()
+            this.hide()
           })
           .catch(() => {
             Methods.showPopup('Load failed', '读取失败', 1000)
@@ -108,6 +134,11 @@ module.exports = {
 </script>
 
 <style lang="stylus" scoped>
+.error
+  position absolute
+  top 0
+  left 0
+
 .debug-wrapper
   position absolute
   inset 10%
@@ -143,7 +174,7 @@ module.exports = {
         cursor pointer
 
   .debug-character
-    font-size 14px
+    font-size 15px
     line-height 14px
     padding 5px 10px
 
